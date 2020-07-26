d5ff800d59c32a2c027d1b64a96ccafcb14f6828
jjeaby <jjeaby@gmail.com>
2020-07-26 23:51:55 +0900

    multi noun extract 작업중

--- SmiToText/find/extract_mecab_multi_noun.py
+++ SmiToText/find/extract_mecab_multi_noun.py
@@ -577,39 +577,51 @@ def extract_file_multi_noun(input, output, item_counter=0):
         line_number += 1
 
 
+
+
 def extract_multi_noun(text, item_counter=0):
-    line = text.strip()
-    line_array_multi_noun_score = {}
-    line_array = line.strip()
 
-    multi_noun_list, multi_noun_list_score = extract_mecab_multi_noun(line_array, item_counter=item_counter)
+    text = text.strip()
 
-    if len(multi_noun_list):
-        for index, word in enumerate(multi_noun_list):
-            if Util().check_email(word):
-                continue
-            else:
-                add_flag = True
-                for char in word:
-                    if char in ["'", "`", ",", "'", "\"", "|", "!", "@", "#", "$", "%", "^", "&", "*", "(",
-                                ")",
-                                "-", "_", "=", "+", "<", ">", ".", ";", ":",
-                                "ㄱ", "ㄴ", "ㄲ", "ㅂ", "ㅃ", "ㅈ", "ㅉ", "ㄷ", "ㄸ", "ㄱ", "ㅁ", "ㅇ", "ㄹ", "ㅎ", "ㅅ",
-                                "ㅆ",
-                                "ㅍ", "ㅊ", "ㅌ", "ㅋ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ", "ㅗ", "ㅓ", "ㅏ", "ㅣ", "ㅠ", "ㅜ",
-                                "ㅡ", " "]:
+    for text_array in text.split("\n"):
+        text_array = text_array.strip()
+
+        line_array_multi_noun_score = {}
+
+        text_array = re.sub(r'[\w.-]+@[\w.-]+.\w+', '', text_array)
+        text_array = re.sub(
+            r'(http|ftp|https)://([\w+?\.\w+])+([a-zA-Z0-9\~\!\@\#\$\%\^\&\*\(\)_\-\=\+\\\/\?\.\:\;\'\,]*)?', '',
+            text_array)
+
+        multi_noun_list, multi_noun_list_score = extract_mecab_multi_noun(text_array, item_counter=item_counter)
+
+        if len(multi_noun_list):
+            for index, word in enumerate(multi_noun_list):
+                if Util().check_email(word):
+                    continue
+                else:
+                    add_flag = True
+                    for char in word:
+                        if char in ["'", "`", ",", "'", "\"", "|", "!", "@", "#", "$", "%", "^", "&", "*", "(",
+                                    ")",
+                                    "-", "_", "=", "+", "<", ">", ".", ";", ":",
+                                    "ㄱ", "ㄴ", "ㄲ", "ㅂ", "ㅃ", "ㅈ", "ㅉ", "ㄷ", "ㄸ", "ㄱ", "ㅁ", "ㅇ", "ㄹ", "ㅎ", "ㅅ",
+                                    "ㅆ",
+                                    "ㅍ", "ㅊ", "ㅌ", "ㅋ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ", "ㅗ", "ㅓ", "ㅏ", "ㅣ", "ㅠ", "ㅜ",
+                                    "ㅡ", " "]:
+                            add_flag = False
+
+                    if word == '기자' or word == str(date.today().day) + '일':
                         add_flag = False
 
-                if word == '기자' or word == str(date.today().day) + '일':
-                    add_flag = False
+                    if check_en_stopword(word):
+                        add_flag = False
 
-                if check_en_stopword(word) :
-                    add_flag = False
+                    if add_flag:
+                        word_score = {word: multi_noun_list_score[word]}
+                        line_array_multi_noun_score.update(word_score)
+                    # print(line_number, word)
 
-                if add_flag:
-                    word_score = {word: multi_noun_list_score[word]}
-                    line_array_multi_noun_score.update(word_score)
-                # print(line_number, word)
     return sorted_dict(line_array_multi_noun_score)
 
 
