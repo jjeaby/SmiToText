83877d9ee934f61a273f9d63557d59cfa3b84f7c
jjeaby <jjeaby@gmail.com>
2020-07-30 22:51:48 +0900

    readme modify

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requires = [
 
 setup(
     name='SmiToText',
-    version='0.1605',
+    version='0.1610',
     py_modules = ['SmiToText', 'SmiToText.font'],
     packages=find_packages(exclude=['tests']),
     package_dir={'SmiToText': 'SmiToText'},
